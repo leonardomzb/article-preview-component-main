fe9c768d3473de9a4e84e50e44522c335dd302c0
leonardomzb <leonardo.zamorano.b@gmail.com>
2024-05-04 14:08:57 -0400

    inital commit

---
+++ scripts/script.js
@@ -0,0 +1,6 @@
+const shareButtonElement = document.querySelector('.article-card_share-button');
+const shareIconsBox = document.querySelector('.article-card_share'); 
+
+shareButtonElement.addEventListener('click', () => {
+  shareIconsBox.classList.toggle('hidden');
+});
