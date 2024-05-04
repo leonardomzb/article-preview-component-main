a56ed3cad3b74182d030e71e109b48a4afe5ac11
leonardomzb <leonardo.zamorano.b@gmail.com>
2024-05-04 14:28:28 -0400

    adding sr-only class

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,7 @@
 const shareButtonElement = document.querySelector('.article-card_share-button');
 const shareIconsBox = document.querySelector('.article-card_share'); 
 
+
 shareButtonElement.addEventListener('click', () => {
   shareIconsBox.classList.toggle('hidden');
 });
